81d3c445a31418047de4faea25ffbcc7fbe4997d
Felix Wiegand <fxwiegand@gmail.com>
2021-05-22 00:15:20 +0200

    Add app update-apps.py and update action

---
+++ update-apps.py
@@ -0,0 +1,18 @@
+import os
+import yaml
+
+with open("_config.yml", 'r') as stream:
+    try:
+        data = yaml.safe_load(stream)
+    except yaml.YAMLError as exc:
+        print(exc)
+
+apps = []
+for file in os.listdir("ctag-tbd/spiffs_image/data/sp"):
+    if file.endswith(".jsn") and file.startswith("mp-"):
+        apps.append(file[3:-4])
+
+data["apps"] = apps
+
+with open("_config.yml", 'w') as outfile:
+    yaml.dump(data, outfile, default_flow_style=False, allow_unicode=True)
